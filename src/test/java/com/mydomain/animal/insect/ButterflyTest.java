bbb824ca90e26bcea9cfb70b16688864031a8805
ngotrunghieu <ngotrunghieu@live.com>
2020-03-25 22:39:54 +0700

    Add more test cases for Butterfly

--- src/test/java/com/mydomain/animal/insect/ButterflyTest.java
+++ src/test/java/com/mydomain/animal/insect/ButterflyTest.java
@@ -7,14 +7,14 @@ import org.junit.jupiter.api.Test;
 class ButterflyTest extends BaseTest {
     @Test
     public void caterpillarCanNotFly() {
-        Butterfly butterfly = new Butterfly();
-        Assertions.assertFalse(butterfly.canFly());
+        Butterfly caterpillar = new Butterfly();
+        Assertions.assertFalse(caterpillar.canFly());
     }
 
     @Test
     public void caterpillarCanWalk() {
-        Butterfly butterfly = new Butterfly();
-        Assertions.assertTrue(butterfly.canWalk());
+        Butterfly caterpillar = new Butterfly();
+        Assertions.assertTrue(caterpillar.canWalk());
     }
 
     @Test
@@ -29,5 +29,20 @@ class ButterflyTest extends BaseTest {
         Butterfly butterfly = new Butterfly();
         butterfly.becomeButterfly();
         Assertions.assertFalse(butterfly.canWalk());
+    }
+
+    @Test
+    public void butterFlyAsButterfly() {
+        Butterfly butterfly = new Butterfly();
+        butterfly.becomeButterfly();
+        butterfly.fly();
+        Assertions.assertEquals("I am flying", outContent.toString().trim());
+    }
+
+    @Test
+    public void caterpillarWalk() {
+        Butterfly caterpillar = new Butterfly();
+        caterpillar.walk();
+        Assertions.assertEquals("I am walking", outContent.toString().trim());
     }
 }
