57de33b6232ba8bc1fe5e9befaee6d39d4412aa0
ngotrunghieu <ngotrunghieu@live.com>
2020-03-25 20:37:18 +0700

    Question A3 - add Rooster

---
+++ src/main/java/com/mydomain/animal/Rooster.java
@@ -0,0 +1,8 @@
+package com.mydomain.animal;
+
+public class Rooster extends Chicken {
+    @Override
+    public void sing() {
+        System.out.println("Cock-a-doodle-doo");
+    }
+}
---
+++ src/test/java/com/mydomain/animal/RoosterTest.java
@@ -0,0 +1,14 @@
+package com.mydomain.animal;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class RoosterTest extends BaseTest {
+    private final Rooster rooster = new Rooster();
+
+    @Test
+    public void roosterSing() {
+        rooster.sing();
+        Assertions.assertEquals("Cock-a-doodle-doo", outContent.toString().trim());
+    }
+}
